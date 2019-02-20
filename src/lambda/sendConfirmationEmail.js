19d9f64a8b566090a5e27734bd175a5d4cd7353b
Muhammad Muhajir <muhajirframe@gmail.com>
2019-02-20 20:27:48 +0700

    Update email template

--- src/lambda/sendConfirmationEmail.js
+++ src/lambda/sendConfirmationEmail.js
@@ -2,7 +2,7 @@ import sgMail from "@sendgrid/mail";
 import Cryptr from "cryptr";
 
 const cryptr = new Cryptr("secret");
-const url = "http://localhost:9000";
+const url = process.env.URL || "http://localhost:9000";
 const addRecipientUrl = `${url}/addRecipient`;
 const getAddRecipientUrl = id => `${addRecipientUrl}?id=${id}`;
 
@@ -19,15 +19,29 @@ exports.handler = async (event, context) => {
   // queryStringParameters – it’ll be in the event body encoded as a query string
   const data = JSON.parse(event.body);
   const id = cryptr.encrypt(JSON.stringify(data));
+  const html = templates.action({
+    title: "Confirm your subscribtion",
+    bodyElements: [
+      "Please confirm your email address by clicking the link below.",
+      "We may need to send you critical information about our service and it is important that we have an accurate email address."
+    ],
+    link: "http://www.mailgun.com/",
+    linkCTA: "Confirm Email Address",
+    linkColor: "#3A90D7",
+    byline: "-- Syria Product",
+    footerText: "Follow ",
+    footerLink: "http://twitter.com/syriaproduct",
+    footerLinkText: "@syriaproduct on Twitter"
+  });
 
   const msg = {
     to: data.email,
     from: "no-reply@muhajirframe.com",
-    subject: "Sending test with sendgrid",
-    text: `Please click this link to subscribe ${getAddRecipientUrl(id)}`,
-    html: `<p>Please click this link to subscribe <br> ${getAddRecipientUrl(
+    subject: "Confirm your subscribtion",
+    text: `Please confirm your email address by clicking the link below.${getAddRecipientUrl(
       id
-    )}</p>`
+    )}`,
+    html
   };
 
   await sgMail.send(msg);
