c02558938a17afc76a07f209ba86d55a5fcefd2e
Muhammad Muhajir <muhajirframe@gmail.com>
2019-03-06 13:52:23 +0700

    Update sendConfirmationEmail.js

--- src/lambda/sendConfirmationEmail.js
+++ src/lambda/sendConfirmationEmail.js
@@ -50,6 +50,10 @@ exports.handler = async (event, context) => {
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
     body: `Please check your email`
   };
 };
